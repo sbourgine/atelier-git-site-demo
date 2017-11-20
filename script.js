b2951916f15ec4b495ea0a518c9c91131c756354
ldu-iRiS <loic.duhaut@systeme-u.fr>
2017-11-20 10:20:44 +0100

    Images en logal

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     var sleepsTo = 0;
     var monthDaysUntil = [359, 328, 300, 269, 239, 208, 178, 147, 116, 86, 55, 25];
-    var picAPic = ["630n0zf4ejjqhb3/snowman-120px.png", "a9v1dtgs9m4g5h3/firestove-120px.png" ,"5uqou2o4ya9agh1/bell-120px.png", "cqn10s10hv9xtne/cookie-120px.png", "k54f3genpvbqr3e/christmasstar-120px.png", "yqpfttxnara7cx7/candy-120px.png", "hhu9jsyhuk39954/ribbon-120px.png", "qqbd9ui5t5wrwoy/stick-120px.png", "f2uaiqo50kecdwl/christmas-tree-120px.png", "mdd0v8rn0zf5dcq/rocket-120px.png", "f9n2w4nfujleve3/rudolph-120px.png", "bmu0m8wxqjvbksz/santacart-120px.png", "ilrzah9sw95ezwr/calendarchristmas-120px.png", "9ox8sbx4s0va0xo/sinterclaus-120px.png"];
+    var picAPic = ["snowman-120px.png", "firestove-120px.png" ,"bell-120px.png", "cookie-120px.png", "christmasstar-120px.png", "candy-120px.png", "ribbon-120px.png", "stick-120px.png", "christmas-tree-120px.png", "rocket-120px.png", "rudolph-120px.png", "santacart-120px.png", "calendarchristmas-120px.png", "sinterclaus-120px.png"];
     var today = new Date();
     
     // add a day to Jan and Feb if a leap year
@@ -27,12 +27,12 @@ $(document).ready(function() {
     if (today.getMonth() == 11 && today.getDate() > 25) {
       sleepsTo = (31 - today.getDate()) + monthDaysUntil[0];
       $('#sleeps').text(sleepsTo);
-      $("#xmasimg").attr("src","https://dl.dropboxusercontent.com/s/" + picAPic[13]);
+      $("#xmasimg").attr("src","third/img/" + picAPic[13]);
     }
     else {
       sleepsTo = monthDaysUntil[today.getMonth()] - today.getDate();
       $('#sleeps').text(sleepsTo);
-      $("#xmasimg").attr("src","https://dl.dropboxusercontent.com/s/" + picAPic[today.getMonth()]);
+      $("#xmasimg").attr("src","third/img/" + picAPic[today.getMonth()]);
     }
     
     // 1 sleep
@@ -44,6 +44,6 @@ $(document).ready(function() {
     if(sleepsTo == 0) {
       $('#sleeps').text("-");
       $('#text').text("Ho, Ho, Ho, it's Christmas!!!");
-      $("#xmasimg").attr("src","https://dl.dropboxusercontent.com/s/" + picAPic[12]);
+      $("#xmasimg").attr("src","third/img/" + picAPic[12]);
     };
   });
