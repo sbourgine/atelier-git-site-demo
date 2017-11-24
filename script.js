9fe8927b585dcee0cadf3e405ff6572f6a924a9d
Sébastien Bourgine <sebastien.bourgine@systeme-u.fr>
2017-11-24 13:35:49 +0100

    Simplification et refactoring pour faciliter la formation

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     
     var sleepsTo = 0;
     var monthDaysUntil = [359, 328, 300, 269, 239, 208, 178, 147, 116, 86, 55, 25];
-    var picAPic = ["snowman-120px.png", "firestove-120px.png" ,"bell-120px.png", "cookie-120px.png", "christmasstar-120px.png", "candy-120px.png", "ribbon-120px.png", "stick-120px.png", "christmas-tree-120px.png", "rocket-120px.png", "rudolph-120px.png", "santacart-120px.png", "calendarchristmas-120px.png", "sinterclaus-120px.png"];
     var today = new Date();
     
     // add a day to Jan and Feb if a leap year
@@ -27,12 +26,12 @@ $(document).ready(function() {
     if (today.getMonth() == 11 && today.getDate() > 25) {
       sleepsTo = (31 - today.getDate()) + monthDaysUntil[0];
       $('#sleeps').text(sleepsTo);
-      $("#xmasimg").attr("src","third/img/" + picAPic[13]);
+      $('#text').text("Sleeps Until Christmas");
     }
     else {
       sleepsTo = monthDaysUntil[today.getMonth()] - today.getDate();
       $('#sleeps').text(sleepsTo);
-      $("#xmasimg").attr("src","third/img/" + picAPic[today.getMonth()]);
+      $('#text').text("Sleeps Until Christmas");
     }
     
     // 1 sleep
@@ -44,6 +43,5 @@ $(document).ready(function() {
     if(sleepsTo == 0) {
       $('#sleeps').text("-");
       $('#text').text("Ho, Ho, Ho, it's Christmas!!!");
-      $("#xmasimg").attr("src","third/img/" + picAPic[12]);
     };
   });
