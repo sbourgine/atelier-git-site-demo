b6937f05de17df207f63191d5e51123571bcbcce
ldu-iRiS <loic.duhaut@systeme-u.fr>
2018-01-01 17:06:03 +0100

    Date fin modifiable

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
     
+    const JOUR_FIN = 25;
+    const MOIS_FIN = 12; // 1-12
+    const LIBELLE = 'Christmas';
+
+    var dateFin = new Date();
+    dateFin.setMonth(MOIS_FIN - 1);
+    dateFin.setDate(JOUR_FIN);
+
+    var now = new Date();
+    if (now > dateFin) {
+      dateFin.setFullYear(now.getFullYear() + 1);
+    }
+
+    console.log(`Now : ${now}`);
+    console.log(`End : ${dateFin}`);
+
     var sleepsTo = 0;
-    var monthDaysUntil = [359, 328, 300, 269, 239, 208, 178, 147, 116, 86, 55, 25];
-    var today = new Date();
-    
-    // add a day to Jan and Feb if a leap year
-    var isLeap = new Date(today.getYear(), 1, 29).getMonth() == 1;
-    if (today.getMonth() == 11 && today.getDate() > 25) {
-      isLeap = new Date((today.getYear()+1), 1, 29).getMonth() == 1;
-    };
-    if(isLeap) {
-      monthDaysUntil[0] = 360;
-      monthDaysUntil[1] = 329;
-    };
-    
+    if (now.getFullYear() === dateFin.getFullYear()) {
+      sleepsTo = numJour(dateFin) - numJour(now);
+      console.log(`_sleepsTo : ${sleepsTo}`);
+    } else {
+      sleepsTo = numJour(dateFin) + nbJoursDansAnnee(now.getFullYear()) - numJour(now);
+      console.log(`_sleepsTo_ : ${sleepsTo}`);
+    }
+
     //set the bg
-    if (today.getMonth() == 11) {
+    if (now.getMonth() == (MOIS_FIN - 1)) {
       $('#mainwrap').addClass('christmas-bg');
     }
     else {
@@ -23,25 +34,29 @@ $(document).ready(function() {
     }
     
     //set the sleeps to Xmas
-    if (today.getMonth() == 11 && today.getDate() > 25) {
-      sleepsTo = (31 - today.getDate()) + monthDaysUntil[0];
-      $('#sleeps').text(sleepsTo);
-      $('#text').text("Sleeps Until Christmas");
-    }
-    else {
-      sleepsTo = monthDaysUntil[today.getMonth()] - today.getDate();
-      $('#sleeps').text(sleepsTo);
-      $('#text').text("Sleeps Until Christmas");
-    }
+    $('#sleeps').text(sleepsTo);
+    $('#text').text("Sleeps Until " + LIBELLE);
     
     // 1 sleep
     if(sleepsTo == 1) {
-      $('#text').text("Sleep Until Christmas");
+      $('#text').text("Sleep Until " + LIBELLE);
     };
     
     //if ITS CHRISTMAS!!!!
     if(sleepsTo == 0) {
       $('#sleeps').text("-");
-      $('#text').text("Ho, Ho, Ho, it's Christmas!!!");
+      $('#text').text("Ho, Ho, Ho, it's " + LIBELLE + "!!!");
     };
   });
+
+  function nbJoursDansAnnee(annee) {
+    return numJour(new Date(annee, 11, 31));
+  }
+
+  function numJour(jour) {
+    var start = new Date(jour.getFullYear(), 0, 0);
+    var diff = jour - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+  }
+  
